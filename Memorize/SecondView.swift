8413f6244e1e4eb99311c374a8c954fe3a445285
krstevskiDarko <darko_krstevski@icloud.com>
2023-09-05 16:21:06 +0200

    add SecondView

---
+++ Memorize/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  Memorize
+//
+//  Created by Darko Krstevski on 5.9.23.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
