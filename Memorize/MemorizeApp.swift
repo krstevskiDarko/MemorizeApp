457c16e496d702eb67779a2c1fdbcd8f011997c0
krstevskiDarko <darko_krstevski@icloud.com>
2023-09-05 16:12:40 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Darko Krstevski on 5.9.23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
