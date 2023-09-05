2ef70a01dd770ecdc7411aec342e9ddddeb91581
krstevskiDarko <darko_krstevski@icloud.com>
2023-09-05 16:15:25 +0200

    First commit! *test*

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,13 +7,15 @@
 
 import SwiftUI
 
+// MARK: body
+
 struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Test for github!")
         }
         .padding()
     }
