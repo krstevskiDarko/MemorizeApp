f9af5d173a91cd9a333879171ce50f06309bdca3
krstevskiDarko <darko_krstevski@icloud.com>
2023-09-06 14:10:38 +0200

    Refactoring ContentView Adding Buttons Adding ScrollViews

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,16 +10,56 @@ import SwiftUI
 
 
 struct ContentView: View {
-    let emojis:[String] = ["💻","⌚️","📱","🖥️","⌨️","💿"]
+    let emojis:[String] = ["💻","⌚️","📱","🖥️","⌨️","💿","🕹️","📷","⏰","🖱️"]
     
+    @State var cardCount = 4
     var body: some View {
-        HStack{
-            ForEach(emojis.indices, id: \.self) { index in
+        VStack{
+            ScrollView{
+                cards
+            }
+            Spacer()
+            cardAdjusters
+        }
+        .foregroundColor(.accentColor)
+        .padding()
+    }
+    
+    var cards: some View{
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))]){
+            ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
-        .foregroundColor(.blue)
+    }
+    var cardAdjusters:some View{
+        HStack{
+            cardRemover
+            Spacer()
+            cardAdder
+        
+        }
         .padding()
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String ) -> some View{
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View{
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    
+    var cardAdder: some View{
+        cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
     }
 }
 
@@ -30,20 +70,22 @@ struct CardView: View{
     var body: some View{
         ZStack {
             let shape = RoundedRectangle(cornerRadius: 12)
-            if isFaceUp{
+            
+            Group {
                 shape.fill(.white)
                 shape.strokeBorder(style:StrokeStyle(lineWidth: 5))
                 Text(content).font(.largeTitle)
             }
-            else{
-                shape
-            }
+            .opacity(isFaceUp ? 1 : 0)
+            
+            shape.fill().opacity(isFaceUp ? 0 : 1)
         }
         .onTapGesture {
             isFaceUp.toggle() //ViewModifier cannot be mutable, the body of View can
         }
     }
 }
+
 
 
 
